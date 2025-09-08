6e86bcbe84d177fc6ec336b07552682aa17366d7
Joseph Heinz <josephheinz28@gmail.com>
2025-09-08 00:35:57 +0000

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<div class="w-full h-full bg-black text-white">
+	{@render children?.()}
+</div>
