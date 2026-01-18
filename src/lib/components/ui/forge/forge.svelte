b6a512abff5488224000e0269df085e08eac203a
Qrexx <josephheinz28@gmail.com>
2026-01-18 15:09:32 -0500

    started enhancer ui

--- src/lib/components/ui/forge/forge.svelte
+++ src/lib/components/ui/forge/forge.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+	import Enhancer from './enhancer.svelte';
 	import Reforger from './reforger.svelte';
 </script>
 
 <div class="size-full">
 	<Reforger />
+	<Enhancer />
 </div>
